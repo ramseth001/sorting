c9762c0fa60ebc846cca494d7667a420411905f9
Sethu <u5849435@anu.edu.au>
2017-05-11 12:24:04 +1000

    Did Quick Sort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,7 +1,79 @@
 package Sorting;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Sethu on 10/5/17.
  */
+
+
 public class QuickSort {
+
+    private static int elements_array[];
+
+    public static void main(String[] args) {
+        ArrayList<Integer> elements = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            elements.add(sc.nextInt());
+        }
+        sc.close();
+
+        elements_array = new int[elements.size()];
+
+        for (int i = 0; i < elements.size(); i++) {
+            elements_array[i] = elements.get(i);
+        }
+
+       doQuickSort( elements.size() - 1,0);
+
+        for (int anElement : elements_array) {
+            System.out.println(anElement);
+        }
+
+    }
+
+    private static void doQuickSort(int high, int low) {
+        // Calculating the middle element
+        int pivot = elements_array[low + ((high - low) / 2)];
+        int i = low;
+        int j = high;
+
+        // There will be two tracking of the array.
+        while (i <= j) {
+            // If the i element is less than the pivot, then the i track iterates over the next element
+            // in the array from its position
+            while (elements_array[i] < pivot) {
+                i++;
+            }
+
+            // If the j element is greater than the pivot, then the j track iterates over the previous element
+            // in the array from its position
+            while (elements_array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                doSwap(i, j);
+                i++;
+                j--;
+            }
+
+        }
+
+        if(low < j){
+            doQuickSort(low,j);
+        }
+        if(i< high){
+            doQuickSort(i,high);
+        }
+    }
+
+
+    private static void doSwap(int i, int j) {
+        int temp = elements_array[i];
+        elements_array[i] = elements_array[j];
+        elements_array[j] = temp;
+    }
 }
